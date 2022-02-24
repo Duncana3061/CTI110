2bc6b1bf0e11fb5bb645adf7f807d16cfde203d0
Duncana3061 <99508349+Duncana3061@users.noreply.github.com>
2022-02-23 23:12:44 -0500

    Add files via upload
    Variable conversions

---
+++ P2LAB1_DuncanAkil.py
@@ -0,0 +1,13 @@
+user_int = int(input('Enter integer (32 - 126):\n'))
+user_float = float(input('Enter float:\n'))
+user_cha = input('Enter character:\n')
+user_text = input('Enter string:\n')
+
+print(user_int, user_float, user_cha, user_text,'\n',user_text, user_cha, user_float, user_int)
+
+print(int(user_int), 'converted to a character is', chr(user_int))
+# FIXME (1): Finish reading other items into variables, then output the four values on a single line separated by a space
+   
+# FIXME (2): Output the four values in reverse
+   
+# FIXME (3): Convert the integer to a characer, and output that character
