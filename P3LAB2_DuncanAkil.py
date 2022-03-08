b399a98470a1d690e5c96978a63f9ba94e0f467d
Duncana3061 <99508349+Duncana3061@users.noreply.github.com>
2022-03-07 22:07:03 -0500

    Add files via upload
    automobile service

---
+++ P3LAB2_DuncanAkil.py
@@ -0,0 +1,18 @@
+service = input("Enter desired auto service:\n")
+print("You entered: " +service)
+
+if service.lower() == "oil change":
+
+   print("Cost of oil change: $35")
+
+elif service.lower() == "car wash":
+
+   print("Cost of car wash: $7")
+
+elif service.lower() == "tire rotation":
+
+   print("Cost of tire rotation: $19")
+
+else:
+
+   print("Error: Requested service is not recognized")
